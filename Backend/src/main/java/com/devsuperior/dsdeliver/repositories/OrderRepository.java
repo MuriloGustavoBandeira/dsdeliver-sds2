f3caf662174100664c87f75340f01202e884deea
Murilo <murilogustavo@hotmail.com>
2021-01-06 20:10:16 -0300

    POST /orders

--- Backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ Backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -10,6 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("SELECT DISTINCT obj FROM ORDER obj JOIN FETCH obj.products "
-            + "WHERE obj.status = 0 ORDER BY obj .moment ASC")
+            + "WHERE obj.status = 0 ORDER BY obj.moment ASC")
     List<Order> findOrdersWithProduct();
 }
