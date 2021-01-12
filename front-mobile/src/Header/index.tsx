68373ab6229b06be0a1d5425e7e00fe1f831dd5a
Murilo <murilogustavo@hotmail.com>
2021-01-12 09:10:43 -0300

    Initial files

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,19 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Header } from 'react-native/Libraries/NewAppScreen';
 
-export default function App() {
+function Header() {
   return (
     <View style={styles.container}>
-      <Header />
-      <StatusBar style="auto" />lsls
+      <Text>DS Delivery</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    backgroundColor: '#DA5C5C',
+    height:90,
   },
 });
+
+export default Header; 
